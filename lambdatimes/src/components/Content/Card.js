21ec47940633f8ec374e68f85b97266af19c9836
borjasolerr <borjasolrey@gmail.com>
2019-02-22 11:18:42 +0100

    Corrected mispelled error in Card.js

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -16,7 +16,7 @@ const Card = props => {
 };
 
 // Make sure to include PropTypes.
-Card.PropTypes = {
+Card.propTypes = {
   data: PropTypes.shape({
     headline: PropTypes.string,
     tab: PropTypes.string,
